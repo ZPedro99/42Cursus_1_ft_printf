c840dbad3edad175cac73c619f3ef25142591810
José Gonçalves <118729517+ZPedro99@users.noreply.github.com>
2022-12-05 14:26:18 +0000

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jomirand <jomirand@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 10:54:30 by jomirand          #+#    #+#             */
+/*   Updated: 2022/12/05 11:37:44 by jomirand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_print_char(char c);
+int	ft_print_string(char *str);
+int	ft_print_numbs(long num);
+int	ft_print_unsigned(unsigned int num);
+int	ft_print_hexa(unsigned long num, char c);
+int	ft_print_address(unsigned long num);
+
+#endif
